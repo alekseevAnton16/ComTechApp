87365329c716f9951580243c9e27641f943a9f19
IMATIC\alekseev.a <alekseev.a@imatic.pro>
2019-04-26 18:38:40 +0300

    Модель факультета

---
+++ src/ComTechNetCoreApp/Models/Faculty.cs
@@ -0,0 +1,12 @@
+﻿namespace ComTechNetCoreApp.Models
+{
+	public class Faculty
+	{
+		public int FacultyId { get; set; }
+
+		/// <summary>
+		/// Наименование факультета.
+		/// </summary>
+		public string FacultyName { get; set; }
+	}
+}
