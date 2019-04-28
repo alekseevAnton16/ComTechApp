258ff59546c8ff7f8d0f2986f79a53be3fd8a2c4
IMATIC\alekseev.a <alekseev.a@imatic.pro>
2019-04-28 21:29:26 +0300

    Соединение с БД, контекст приложения

---
+++ src/ComTechNetCoreApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ComTechNetCoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComTechNetCoreApp.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<Lecturer> Lecturers { get; set; }
+		public DbSet<Subdivision> Subdivisions { get; set; }
+
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
--- src/ComTechNetCoreApp/Startup.cs
+++ src/ComTechNetCoreApp/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using ComTechNetCoreApp.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,6 +19,9 @@ namespace ComTechNetCoreApp
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
 			services.AddMvc();
 		}
 
